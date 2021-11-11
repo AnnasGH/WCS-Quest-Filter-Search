4e2cca25cc2159d4b61c47fd84834ba6b8546df9
AnnasGH <48451437+AnnasGH@users.noreply.github.com>
2021-11-11 15:53:56 +0100

    start point: almost good solution

--- src/App.js
+++ src/App.js
@@ -13,19 +13,14 @@ export default function App() {
     "garin"
   ]);
 
-  console.log(`1: ${searchValue}`);
-
   let tmpArray = [];
   tmpArray = users.filter((user) => user === searchValue);
-
-  console.log(tmpArray);
 
   return (
     <div className="App">
       <h1>Users List</h1>
       <h2>Search for your favourite user!</h2>
       <Search searchValue={searchValue} setSearchValue={setSearchValue} />
-      {console.log(`2:${searchValue}`)}
 
       {searchValue === ""
         ? users.map((user) => <p>{user}</p>)
